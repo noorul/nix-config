aa4ad79885450f659bea5dc0a4e92fdfa786253c
Noorul Islam K M <noorul@noorul.com>
2025-08-31 22:03:34 +0530

    Add NVM settings

--- flake.nix
+++ flake.nix
@@ -199,6 +199,9 @@
                     # Load secrets at shell startup
                     eval $(/opt/homebrew/bin/brew shellenv)
                     source /Users/noorul/.secrets
+                    export NVM_DIR="$HOME/.config/nvm"
+                    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+                    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
                   '';
                 };
                 programs.starship.enable = true;
